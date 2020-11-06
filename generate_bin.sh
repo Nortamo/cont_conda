1b3e659d4aed5f13ecc67149c99e563be0fbf5c3
Nortamo <40563680+Nortamo@users.noreply.github.com>
2020-11-06 16:04:18 +0200

    fix relative execution

--- generate_bin.sh
+++ generate_bin.sh
@@ -18,7 +18,9 @@ executables="$(ls -p | grep -v /)"
 cd $BASE_DIR
 
 
-PRE_COMMAND="source ../common.sh"
+REL_PATH_CMD='DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"'
+
+PRE_COMMAND="source \$DIR/../common.sh"
 _BIND_FLAGS="-B /users:/users  -B /projappl:/projappl -B /scratch:/scratch"
 echo "export _BIND_FLAGS=\"$_BIND_FLAGS\"" > deploy/common.sh
 echo "export _SQUASH_FS_NAME=$_SQUASH_FS_NAME" >> deploy/common.sh
@@ -27,8 +29,9 @@ echo "export _IMG_NAME=\"$_IMG_NAME\"" >> deploy/common.sh
 if [[ "$WRAPPERS" = true  ]]; then
 while IFS= read -r executable; do
     cmd=$(echo "$TARGET//bin/$executable" | sed 's@//*@/@g')
-    RUN_CMD="singularity exec \$_BIND_FLAGS --overlay=../\$_SQUASH_FS_NAME ../\$_IMG_NAME $cmd \$@"
+    RUN_CMD="singularity exec \$_BIND_FLAGS --overlay=\$DIR/../\$_SQUASH_FS_NAME \$DIR/../\$_IMG_NAME $cmd \$@"
     echo "#!/bin/bash" > deploy/bin/$executable
+    echo "$REL_PATH_CMD" >> deploy/bin/$executable
     echo "$PRE_COMMAND" >> deploy/bin/$executable
     echo $RUN_CMD >> deploy/bin/$executable
     chmod +x deploy/bin/$executable
@@ -37,7 +40,7 @@ fi
 
 cp $_IMG_NAME deploy
 cp $_SQUASH_FS_NAME deploy
-
+chmod o+rx -R deploy
 
 fusermount -u mount_dir
 rm -rf mount_dir

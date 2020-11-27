3c753ca048a7a062a97b111bafc954641bfb4d6b
Nortamo <40563680+Nortamo@users.noreply.github.com>
2020-11-27 13:31:53 +0200

    Adds debug exec and test for local disk before mout

--- generate_bin.sh
+++ generate_bin.sh
@@ -26,13 +26,14 @@ REL_PATH_CMD='DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && p
 
 PRE_COMMAND="source \$DIR/../common.sh"
 #_BIND_FLAGS="-B /users:/users  -B /projappl:/projappl -B /scratch:/scratch"
-_BIND_FLAGS=$(ls -1 / | awk '!/dev/'  | sed 's/^/\//g'  | awk '{ print $1,$1 }' | sed 's/ /:/g' | sed 's/^/-B /g' | tr '\n' ' ')
+_BIND_FLAGS=$(ls -1 / | awk '!/dev/' | awk '!/local_scratch/'  | sed 's/^/\//g'  | awk '{ print $1,$1 }' | sed 's/ /:/g' | sed 's/^/-B /g' | tr '\n' ' ')
 _BIND_FLAGS="$_BIND_FLAGS -B \$DIR/../MASK:$MASK_DIR -B \$DIR/../MASK:\$DIR/../bin"
 echo "export _BIND_FLAGS=\"$_BIND_FLAGS\"" > deploy/common.sh
 echo "export _SQUASH_FS_NAME=$_SQUASH_FS_NAME" >> deploy/common.sh
 echo "export _IMG_NAME=\"$_IMG_NAME\"" >> deploy/common.sh
 echo "export SINGULARITYENV_PATH=$TARGET/bin:\$PATH" >> deploy/common.sh
 echo "export SINGULARITYENV_LD_LIBRARY_PATH=$TARGET/lib/:\$LD_LIBRARY_PATH" >> deploy/common.sh
+echo "test -d /local_scratch/ && export _BIND_FLAGS=\"\$_BIND_FLAGS -B /local_scratch:/local_scratch\"" >> deploy/common.sh
 
 # Dump extra environment variables
 printenv | grep _EXTRA_ENV_ | sed 's/_EXTRA_ENV_/export /g' >> deploy/common.sh
@@ -52,6 +53,14 @@ fi
 
 executable="_debug_shell"
 RUN_CMD="singularity --silent shell \$_BIND_FLAGS --overlay=\$DIR/../\$_SQUASH_FS_NAME \$DIR/../\$_IMG_NAME \$@"
+echo "#!/bin/bash" > deploy/bin/$executable
+echo "$REL_PATH_CMD" >> deploy/bin/$executable
+echo "$PRE_COMMAND" >> deploy/bin/$executable
+echo $RUN_CMD >> deploy/bin/$executable
+chmod +x deploy/bin/$executable
+
+executable="_debug_exec"
+RUN_CMD="singularity --silent exec \$_BIND_FLAGS --overlay=\$DIR/../\$_SQUASH_FS_NAME \$DIR/../\$_IMG_NAME \$@"
 echo "#!/bin/bash" > deploy/bin/$executable
 echo "$REL_PATH_CMD" >> deploy/bin/$executable
 echo "$PRE_COMMAND" >> deploy/bin/$executable
